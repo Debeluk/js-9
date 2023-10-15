60368a5ef4bc154e8c08bba34ae330eb4040e44b
Debeluk <popcorn.starinets@gmail.com>
2023-10-15 16:46:53 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let timespan;
+
+document.getElementById('startButton').addEventListener('click', function () {
+  if (!timespan) {
+    this.disabled = true;
+
+    document.getElementById('stopButton').disabled = false;
+
+    timespan = setInterval(function () {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
