ecb1ff9560470e5f3b4a50533b0989005d83ad76
Debeluk <popcorn.starinets@gmail.com>
2023-10-17 19:30:59 +0300

    all

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.target.elements;
+
+  for (let i = 0; i < amount.value; i++) {
+    const currentDelay = Number(delay.value) + Number(step.value) * i;
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
   }
-}
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(
+      () =>
+        shouldResolve
+          ? resolve({ position, delay })
+          : reject({ position, delay }),
+      delay
+    );
+  });
+};
